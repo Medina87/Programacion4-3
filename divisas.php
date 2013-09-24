12d446f7504292213acf912d87a775f9f4a2867f
Medina87 <duck.medina38@gmail.com>
2013-09-23 21:15:22 -0500

    Signed-off-by: Medina87 <duck.medina38@gmail.com>

---
+++ divisas.php
@@ -0,0 +1,8 @@
+<?php
+$cantidad = 5; 
+$paises = array ("",""); 
+$cambios = array (12.81); 
+echo $cantidad." dolares conversión a peso mexicano: "; 
+for ($i=0; $i<1; $i++) 
+echo $paises[$i]." ".$cambios[$i]*$cantidad."<br />"; 
+?>
